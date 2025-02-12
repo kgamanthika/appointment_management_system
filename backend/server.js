eea6781efd97899bf0c6a9af08eb51093276df4d
Amesh Manthika <166608158+kgamanthika@users.noreply.github.com>
2025-02-12 21:06:09 +0530

    update srever.js

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
-const db = require('./config/db');
+const authRoutes = require('./routes/auth');
 const appointmentRoutes = require('./routes/appointments');
-
-dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
-app.use('/api', appointmentRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/appointments', appointmentRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(5000, () => console.log('Server running on port 5000'));
